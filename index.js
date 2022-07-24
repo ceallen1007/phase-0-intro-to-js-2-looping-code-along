87066c852443fa5ca5f6ef9e35484930078a3bfd
charlieallen <ceallen1007@gmail.com>
2022-07-23 22:08:08 -0500

    commit tree 4c90fe2c054ca369d40e53f2857ade99d7e20f43

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+const writeCardsArray = [] 
+
+function writeCards(names) {
+  for (let i = 0; i < names.length; i++) {
+    writeCardsArray.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    
+  }
+
+  return writeCardsArray 
+
+
+}
+
+function countDown(number){
+    while (number >= 0){
+    console.log(number)
+    number--
+    }
+
+}
